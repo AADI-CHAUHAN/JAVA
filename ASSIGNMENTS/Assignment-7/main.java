43e785e8c62c394cff769b321949c1bec028b616
AADI-CHAUHAN <chauhanaaditiya4@gmail.com>
2025-11-15 16:54:26 +0530

    Add files via upload

---
+++ ASSIGNMENTS/Assignment-7/main.java
@@ -0,0 +1,9 @@
+import exercise.java.packagedemo;
+
+public class main{
+public static void main(String args[]){
+packagedemo objn = new packagedemo();
+objn.display();
+System.out.println("AADITIYA CHAUHAN 24CSU362");
+}
+}
